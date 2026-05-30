97b8c5b6ff75ac01381427030690b4e64ee1e3e0
whitewalker <flico@localhost.localdomain>
2026-05-30 12:38:31 -0300

    Estrutura inicial com escopo do trabalho

---
+++ files/index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* 
+ 1. Gerar senhas com parâmetros ( tamanho, tipos de caracteres ) 
+ 2. validar força
+ 3. salvar em arquivo opcional
+  */
+
+int main(){
+
+	return 0;
+}
