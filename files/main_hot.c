352dd7bc6acb7cd400dd4abc7320a6b9ac1beba6
whitewalker <flico@localhost.localdomain>
2026-06-14 02:13:49 -0300

    adicionar calcular universo e bits necessários

--- files/main_hot.c
+++ files/main_hot.c
@@ -1,13 +1,8 @@
+#include <stdint.h> 
 #include <stdio.h> 
 #include <sys/random.h> 
 #include <string.h> 
-
-typedef struct {
-	int numeros;
-	int minusculas;
-	int maiusculas; 
-	int especiais; 
-} Preferencias;  
+#define SENHA_MAX 128
 
 typedef struct { 
 	const char *nome; 
@@ -15,6 +10,8 @@ typedef struct {
 	size_t tamanho; 
 	int habilitado;
 } Conjunto; 
+size_t calcular_universo(Conjunto conjuntos[]);
+size_t bits_necessarios(size_t universo);
 
 /* Cada campo representa: 
  	nome -> descrição do conjunto;
@@ -89,12 +86,11 @@ int main() {
 	//printf("\nOs valores preenchidos são: %d, %d, %d e %d. ", p.numeros, p.minusculas, p.maiusculas, p.especiais); 
 
 	printf("\nDigite o tamanho da senha, deverá ser entre 16 e 128 caracteres: "); 
-	while(scanf("%d", &tamanho) != 1 || tamanho < 16 || tamanho > 128){ 
+	while(scanf("%d", &tamanho) != 1 || tamanho < 16 || tamanho > SENHA_MAX){ 
 		printf("\nDigite um valor válido: "); 
 		while(getchar() != '\n'); 
 	} 
 	size_t bytes = getrandom(buff, sizeof(buff), 0); 
-
 	if(bytes == -1){ 
 		perror("getrandom"); 
 		return 1; 
@@ -105,12 +101,43 @@ int main() {
 		printf("%02X", buff[i]); 
 	}
 	printf("\n"); 
+
 		
-	size_t universo;
+	
+	size_t universo = calcular_universo(conjuntos); 
+	size_t bits = bits_necessarios(universo);
+	size_t valor; // indice sorteado
+
+	uint64_t mascara= (1ULL << bits) -1;// formato 64bits com atribuição dos bits menos significativos. unsignedlonglong 	
+	//valor = pool & mascara; 
+	//valor = buff[i] % universo; 
+	uint64_t pool= 0; 
+	int bits_disponiveis= 0;
+	printf("\n%zu ", universo); 
+	//size_t calcular_universo(),int  bits_necessários(),char obter_caractere(), gerar_senha 
+	return 0;
+}
+size_t calcular_universo(Conjunto conjuntos[]) 
+{ 
+size_t universo;
 
 	for(int i= 0;i < 4; i++){ 
 		if(conjuntos[i].habilitado){ 
 			universo+= conjuntos[i].tamanho; 
-		} 
-	return 0;
+		}
+	}
+	return universo; 
+} 
+
+size_t bits_necessarios(size_t universo){ 
+	// Os valores do universo são calculados conforme a base 2, se 2⁶ é 64 e universo possui 62, ele ira retornar com 6, pois é o menor número de bits necessários para representar todos os números do universo. 2^(bits).
+	size_t bits= 0;
+	size_t valor= 1;
+
+	while(valor < universo)
+	{ 
+		valor <<= 1;
+		bits++; 
+	} 
+	return bits;
 } 
